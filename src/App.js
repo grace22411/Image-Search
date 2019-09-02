c1efe311bf72b397117cbd7864240eefd210699a
grace22411 <olayinkagrace112.ob@gmail.com>
2019-09-02 16:02:20 -0700

    image search

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import photo from "./photo-camera.png";
+import search from "./search.png";
+import {
+  InstantSearch,
+  SearchBox,
+  InfiniteHits
+} from 'react-instantsearch/dom';
+import Upload from "./upload";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      file: null
+    }
+    this.onChangeFile = this.onChangeFile.bind(this)
+  }
+  onChangeFile = (event)=> {
+    console.log('event.target.files[0]', event.target.files[0])
+    event.stopPropagation();
+    event.preventDefault();
+    var file = event.target.files[0];
+    console.log('file.....:', file);
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0])
+    })
+  }
+  render(){
+    return (
+      <div className="container">
+          <header>
+            <p>Settings</p>
+          </header>
+
+          <div className="search-container">
+            <h1>Image<span>Search</span></h1>
+            <div className="input-search">
+              <input type="text" placeholder="|" 
+             />
+              <input type="file" id="file" style={{display:"none"}}
+              onChange={(e) => this.onChangeFile(e)} />
+               
+              <label htmlFor="file" >
+                <img src={photo} alt="camera" />
+              </label>
+              <img src={search} className="search" alt="search icon"/>
+             
+            
+            </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
